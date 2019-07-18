7ad7492bdd10ae7a3052808587225879b95a14fe
Camille Prost <cprost.com@gmail.com>
2019-07-18 11:44:01 +0200

    Commit initial du back

---
+++ src/main/java/be/businesstraining/myimmowebback/MyimmowebbackApplication.java
@@ -0,0 +1,13 @@
+package be.businesstraining.myimmowebback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyimmowebbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyimmowebbackApplication.class, args);
+    }
+
+}
